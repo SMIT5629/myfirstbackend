1841830cfe448e061a9902a464f9c4c7769e34aa
SMIT5629 <smitrpatel05@outlook.com>
2025-06-19 18:25:28 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.urlencoded({extended:true, limit: "10mb"}));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+
+// routes import 
+import userRouter from "./routes/user.routes.js"
+
+//route declaration
+app.use("/api/v1/user",userRouter)
+
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,4 @@
 const asynchnandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 }
-export { asynchnandler}
+export default asynchnandler;
